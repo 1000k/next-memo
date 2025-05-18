798168b38092e139b3ba9f8cb14a29f97c6f7577
SENDA Keijiro <senda.keijiro@gmail.com>
2025-05-18 21:47:09 +0900

    feat: Redirect to "/memo" from top page when logged in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 import Link from 'next/link';
-import SignIn from './components/sign-in';
+import SignIn from '@/app/components/sign-in';
+import { auth } from '@/../auth';
+import { redirect } from 'next/navigation';
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+  if (session) {
+    redirect('/memo');
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="text-3xl font-bold mb-4">Next-memo</h1>
@@ -9,9 +16,7 @@ export default function Home() {
       <Link
         href="/memo"
         className="text-blue-500 underline"
-      >
-        write
-      </Link>
+      ></Link>
     </main>
   );
 }
