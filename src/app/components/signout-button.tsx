732b329344933ce595479b6845d8219cf1318d23
SENDA Keijiro <senda.keijiro@gmail.com>
2025-05-18 21:18:39 +0900

    Refine sign out button

--- src/app/components/signout-button.tsx
+++ src/app/components/signout-button.tsx
@@ -10,7 +10,13 @@ export function SignOut() {
     >
       <button
         type="submit"
-        className="rounded-md bg-red-900 px-4 py-2 text-sm font-medium text-gray shadow-sm hover:bg-red-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
+        className="text-sm text-blue-500 bg-transparent p-0 border-none hover:text-blue-300 focus:outline-none"
+        style={{
+          background: 'none',
+          border: 'none',
+          padding: 0,
+          cursor: 'pointer',
+        }}
       >
         Sign Out
       </button>
