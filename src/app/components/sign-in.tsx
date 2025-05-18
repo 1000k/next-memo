b9eea21ebc5f97f1e56919bb281bbf5ce63e2a69
SENDA Keijiro <senda.keijiro@gmail.com>
2025-05-18 14:20:52 +0900

    Add Google sign-in button (not functional yet)

---
+++ src/app/components/sign-in.tsx
@@ -0,0 +1,19 @@
+import { signIn } from '@/../auth';
+
+export default function SignIn() {
+  return (
+    <form
+      action={async () => {
+        'use server';
+        await signIn('google');
+      }}
+    >
+      <button
+        type="submit"
+        className="flex w-full items-center justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+      >
+        Signin with Google
+      </button>
+    </form>
+  );
+}
