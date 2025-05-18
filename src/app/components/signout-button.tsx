bf94d776f67c067ac8881e100499b70e1829afb7
SENDA Keijiro <senda.keijiro@gmail.com>
2025-05-18 21:16:31 +0900

    Refine header design

--- src/app/components/signout-button.tsx
+++ src/app/components/signout-button.tsx
@@ -8,7 +8,12 @@ export function SignOut() {
         await signOut();
       }}
     >
-      <button type="submit">Sign Out</button>
+      <button
+        type="submit"
+        className="rounded-md bg-red-900 px-4 py-2 text-sm font-medium text-gray shadow-sm hover:bg-red-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
+      >
+        Sign Out
+      </button>
     </form>
   );
 }
--- src/app/memo/page.tsx
+++ src/app/memo/page.tsx
@@ -5,23 +5,31 @@ import { SignOut } from '../components/signout-button';
 export default function Memo() {
   return (
     <div className="md:container md:max-w-screen-md p-4">
-      <form action="">
-        <input
-          id="memo"
-          type="text"
-          placeholder="Input your memo here..."
-          className="border-2 border-gray-300 rounded-md p-2 mb-4 w-8/12"
-          required
-        />
-        <button
-          type="submit"
-          className="bg-blue-500 text-white rounded-md p-2 ml-4 hover:bg-blue-600"
+      <header className="flex items-center justify-between mb-4">
+        <form
+          action=""
+          className="flex w-2/3 items-center"
         >
-          add
-        </button>
-        <UserAvatar />
-      </form>
-      <SignOut />
+          <input
+            id="memo"
+            type="text"
+            placeholder="Input your memo here..."
+            className="border-2 border-gray-300 rounded-md p-2 w-full"
+            required
+          />
+          <button
+            type="submit"
+            className="bg-blue-500 text-white rounded-md p-2 ml-4 hover:bg-blue-600"
+          >
+            add
+          </button>
+        </form>
+        <div className="flex w-1/3 items-center justify-end gap-2">
+          <UserAvatar />
+          <SignOut />
+        </div>
+      </header>
+
       <ul>
         {Array.from({ length: 10 }).map((_, i) => (
           <li
