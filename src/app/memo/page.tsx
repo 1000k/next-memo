fdd491b0bc98946ae9173bbc3ca522b5ea1f9774
SENDA Keijiro <senda.keijiro@gmail.com>
2025-05-20 06:51:34 +0900

    feat: display icons instead of text

--- src/app/memo/page.tsx
+++ src/app/memo/page.tsx
@@ -31,14 +31,23 @@ export default async function Memo() {
             name="title"
             type="text"
             placeholder="Input your memo here..."
-            className="border-2 border-gray-300 rounded-md p-2 w-full"
+            className="border-2 border-gray-400 rounded-md p-2 w-full focus:outline-none focus:border-blue-600 focus:ring-1 focus:ring-blue-600"
             required
           />
           <button
             type="submit"
-            className="bg-blue-500 text-white rounded-md p-2 ml-4 hover:bg-blue-600"
+            className="text-gray-400 transition duration-150 ease-in-out rounded-md p-2 ml-4 hover:bg-gray-700 hover:text-white"
           >
-            add
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="size-6"
+            >
+              <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32l8.4-8.4Z" />
+              <path d="M5.25 5.25a3 3 0 0 0-3 3v10.5a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3V13.5a.75.75 0 0 0-1.5 0v5.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5V8.25a1.5 1.5 0 0 1 1.5-1.5h5.25a.75.75 0 0 0 0-1.5H5.25Z" />
+              <title>Add</title>
+            </svg>
           </button>
         </form>
         <div className="flex w-1/3 items-center justify-end gap-2">
