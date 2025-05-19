0e602562115bf418d1f661b6e8292271bf17cc34
SENDA Keijiro <senda.keijiro@gmail.com>
2025-05-20 00:28:33 +0900

    fix(signout): fix URL after redirect

--- src/app/components/signout-button.tsx
+++ src/app/components/signout-button.tsx
@@ -1,25 +1,20 @@
-import { signOut } from '@/auth';
+'use client';
+import { signOut } from 'next-auth/react';
 
 export function SignOut() {
   return (
-    <form
-      action={async () => {
-        'use server';
-        await signOut();
+    <button
+      type="button"
+      onClick={() => signOut({ callbackUrl: '/' })}
+      className="text-sm text-blue-500 bg-transparent p-0 border-none hover:text-blue-300 focus:outline-none"
+      style={{
+        background: 'none',
+        border: 'none',
+        padding: 0,
+        cursor: 'pointer',
       }}
     >
-      <button
-        type="submit"
-        className="text-sm text-blue-500 bg-transparent p-0 border-none hover:text-blue-300 focus:outline-none"
-        style={{
-          background: 'none',
-          border: 'none',
-          padding: 0,
-          cursor: 'pointer',
-        }}
-      >
-        Sign Out
-      </button>
-    </form>
+      Sign Out
+    </button>
   );
 }
