d35560d4f0c90f86241177fc3687f5a93791fe62
muratagawa <senda.keijiro@gmail.com>
2025-05-23 07:45:12 +0900

    feat: add metadata

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,16 @@
 import SignIn from '@/components/sign-in';
 
+export const metadata = {
+  title: 'Login - next-memo',
+  description: 'Login to next-memo',
+  openGraph: {
+    title: 'Login - next-memo',
+    description: 'Login to next-memo',
+  },
+};
+
 export default async function Login() {
+
   return (
     <main className="flex h-svh flex-col items-center justify-center p-24">
       <h1 className="text-3xl font-bold mb-4">next-memo</h1>
