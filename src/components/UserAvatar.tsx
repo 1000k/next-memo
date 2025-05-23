0f805edaa9928c343dbedac25b9e1ca57b356680
muratagawa <senda.keijiro@gmail.com>
2025-05-23 11:03:48 +0900

    feat(UserAvatar): close the dropdown when clicking outside

--- src/components/UserAvatar.tsx
+++ src/components/UserAvatar.tsx
@@ -1,16 +1,30 @@
 'use client';
 
 import Image from 'next/image';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { handleSignOut } from '@/actions/actions';
 import { SignOutIcon } from '@/components/icons';
 
 export default function UserAvatar({ image }: { image?: string }) {
-  const [open, setOpen] = useState(false);
+  const [isOpen, setOpen] = useState(false);
 
   const toggleOpen = () => {
     setOpen((open) => !open);
   };
+
+  // Add event listener to close the dropdown when clicking outside
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      const target = event.target as HTMLElement;
+      if (isOpen && target.closest('.dropdown') === null) {
+        setOpen(false);
+      }
+    };
+    document.addEventListener('click', handleClickOutside);
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [isOpen]);
 
   return (
     <div className="relative flex items-center justify-center">
@@ -22,8 +36,8 @@ export default function UserAvatar({ image }: { image?: string }) {
         alt="User Avatar"
         onClick={() => toggleOpen()}
       />
-      {open && (
-        <div className="absolute top-10 right-0 mt-2 w-40 z-10">
+      {isOpen && (
+        <div className="absolute top-10 right-0 mt-2 w-40 z-10 dropdown">
           <button
             className="inline-flex p-4 text-gray-100  bg-gray-700 shadow-lg border-none hover:text-white hover:bg-gray-600 w-full rounded-md text-left"
             type="button"
